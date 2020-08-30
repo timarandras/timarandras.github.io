9a500243b0629501404602c7ef966d7fbdaeadfe
Andras Timar <andras.timar@c3s.hu>
2020-08-30 03:11:24 +0200

    Fiddle with styles, theme fonts.

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,10 +5,19 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 import { Container } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Layout = ({ children }) => <Container maxWidth="md">{children}</Container>;
+const theme = {
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+};
+
+const Layout = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <Container maxWidth="lg">{children}</Container>
+  </ThemeProvider>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,70 @@
+import 'fontsource-roboto';
+
+import { Card, CardContent, Grid, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="About me" />
-    <h1>András Timár, PhD</h1>
-    <h2>Soon...</h2>
-  </Layout>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  Card: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const IndexPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Layout>
+      <SEO title="About me" />
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Card className={classes.Card}>
+            <CardContent>
+              <Typography className={classes.title} color="textSecondary" gutterBottom>
+                Word of the Day
+              </Typography>
+              <Typography variant="h5" component="h2">
+                be nevo lent
+              </Typography>
+              <Typography variant="body2" component="p">
+                well meaning and kindly.
+                <br />
+                "a benevolent smile"
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs>
+          <Card className={classes.Card}>xs</Card>
+        </Grid>
+        <Grid item xs>
+          <Card className={classes.Card}>xs</Card>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Card className={classes.Card}>xs</Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card className={classes.Card}>xs=6</Card>
+        </Grid>
+        <Grid item xs>
+          <Card className={classes.Card}>xs</Card>
+        </Grid>
+      </Grid>
+
+      <h1>András Timár, PhD</h1>
+      <h2>Soon...</h2>
+    </Layout>
+  );
+};
 
 export default IndexPage;
