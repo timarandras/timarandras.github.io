6f71f58efd5fe723b5526e8fb154b16866035012
Andras Timar <andras.timar@c3s.hu>
2020-11-21 18:19:44 +0100

    Add last updated time ago. Update deps. Closes #7.

--- src/components/SourceCard.jsx
+++ src/components/SourceCard.jsx
@@ -1,9 +1,14 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Card, CardContent, Link, Typography } from '@material-ui/core';
+import useTimeAgo from '@rooks/use-time-ago';
 import React from 'react';
 
+const date = new Date();
+
 export default function SourceCard() {
+  // @ts-ignore
+  const lastUpdated = useTimeAgo(date.getTime());
   return (
     <Card raised>
       <CardContent>
@@ -17,6 +22,11 @@ export default function SourceCard() {
             {/* eslint-disable react/jsx-one-expression-per-line */}
             View source on <FontAwesomeIcon icon={faGithub} />.{/* eslint-ensable react/jsx-one-expression-per-line */}
           </Link>
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography align="center" color="textSecondary" variant="body1">
+          Last updated {lastUpdated}
         </Typography>
       </CardContent>
     </Card>
